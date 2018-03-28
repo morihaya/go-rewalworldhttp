e3b5e6a8c8461c9f84310770bb93ad980e027012
morihaya <yukiya.hayashi@gmail.com>
2018-03-28 16:05:14 +0900

    fix: typo backslash

--- 1.1.1/main.go
+++ 1.1.1/main.go
@@ -14,7 +14,7 @@ func handler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	fmt.Println(string(dump))
-	fmt.Fprintf(w, "<html><body>hello</body></html>¥n")
+	fmt.Fprintf(w, "<html><body>hello</body></html>\n")
 }
 
 func main() {
